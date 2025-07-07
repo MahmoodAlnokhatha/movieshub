647a202bdf19776d52f2c8bb1a04635034a70feb
Mahmoodalnokhatha <mahmoodalnokhtha@gmail.com>
2025-07-07 16:41:50 +0300

    roger

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,7 +28,7 @@ router.get("/sign-in", (req, res) => {
 });
 
 router.post("/sign-in", async (req, res) => {
-  const userInDatabase = await User.findOne({ username: req.body.username });
+  const userInDatabase = await User.findOne({ username: new RegExp(`^${req.body.username}$`, 'i') });
   if (!userInDatabase) {
     return res.send("Login Failed. Please try again later");
   }
