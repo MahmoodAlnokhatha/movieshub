574939fd06cf4b85bc2c183c08f381f2cfc9d119
Mahmoodalnokhatha <mahmoodalnokhtha@gmail.com>
2025-07-07 04:41:50 +0300

    second update

---
+++ server.js
@@ -0,0 +1,45 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require("express-session");
+
+const passUserToView = require("./middleware/pass-user-to-view");
+const isSignedIn = require("./middleware/is-signed-in");
+
+const authController = require("./controllers/auth");
+const moviesController = require("./controllers/movies");
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passUserToView);
+
+app.set("view engine", "ejs");
+
+app.use("/auth", authController);
+app.use("/movies", isSignedIn, moviesController);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
